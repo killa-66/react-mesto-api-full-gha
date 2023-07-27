c84dc00387d82f1a665e993230336e4f6640fb08
Yaroslav Molochaev <killa66@MacBook-Pro-Yaroslav.local>
2023-07-27 12:05:26 +0300

    fix: cors2

--- backend/express-mesto-gha/middlewares/cors.js
+++ backend/express-mesto-gha/middlewares/cors.js
@@ -9,7 +9,7 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   const { method } = req;
   if (method === 'OPTIONS') {
